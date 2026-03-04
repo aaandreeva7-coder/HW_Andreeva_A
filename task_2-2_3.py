ca2a33efd62a24f0107f86319e2db990ccf9dc4a
aaandreeva7-coder <aaandreeva7@gmail.com>
2026-03-05 01:16:31 +0300

    Add files via upload

---
+++ task_2-2_3.py
@@ -0,0 +1,25 @@
+device_name_1 = "Поляриметр"
+inventory_number_1 = "INV-2024-077"
+is_operational_1 = "исправен"
+quantity_1 = 3
+
+device_name_2 = "Холодильная установка"
+inventory_number_2 = "INV-2025-103"
+is_operational_2 = "неисправен"
+quantity_2 = 1
+
+device_name_3 = "Микроскоп"
+inventory_number_3 = "INV-2023-077"
+is_operational_3 = "исправен"
+quantity_3 = 2
+
+device_name_4 = "Спектрофотометр"
+inventory_number_4 = "INV-2025-126"
+is_operational_4 = "исправен"
+quantity_4 = 1
+
+print("Наименование\tИнвентарный №\t\tСостояние\tКоличество")
+print(f"{device_name_1}\t\t{inventory_number_1}\t\t{is_operational_1}\t{quantity_1}")
+print(f"{device_name_2}\t{inventory_number_2}\t{is_operational_2}\t{quantity_2}")
+print(f"{device_name_3}\t\t{inventory_number_3}\t\t{is_operational_3}\t{quantity_3}")
+print(f"{device_name_4}\t{inventory_number_4}\t\t{is_operational_4}\t{quantity_4}")
