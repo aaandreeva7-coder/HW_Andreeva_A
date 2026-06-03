d596aa8627ccfb4a726606fc7c275ae2896d58a7
aaandreeva7-coder <aaandreeva7@gmail.com>
2026-06-04 02:33:50 +0300

    Delete task_2-0_6.py

--- task_2-0_6.py
+++
@@ -1,5 +0,0 @@
-with open("C:/Users/Andrey/Desktop/pycharm/output.txt", "w", encoding="utf-8") as f:
-    print("Андреева Алина Андреевна", file=f)
-    print("СПБПУ", file=f)
-    print ("Биотехнология", file=f)
-    print("4731901/50001", file=f)
