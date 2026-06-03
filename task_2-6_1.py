444c88f49cfb3ba992cdd5a42fd1bd7d3b090959
aaandreeva7-coder <aaandreeva7@gmail.com>
2026-06-04 02:39:53 +0300

    Delete task_2-6_1.py

--- task_2-6_1.py
+++
@@ -1,9 +0,0 @@
-
-pH = float(input("\nВведите значение pH: "))
-
-if pH < 7:
-    print("\n\tКислая среда")
-elif pH == 7:
-    print("\n\tНейтральная среда")
-else:
-    print("\n\tЩелочная среда")
